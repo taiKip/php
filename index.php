b159a8b42c258f1d12522072ecd0be377eec98a8
tarus <victortarus6@gmail.com>
2025-08-20 11:49:51 +0930

    variables done

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $color = "red";
+    $txt = "cars";
+    echo "I love ". $txt . "<br>";
+        echo "My car is " . $color . "<br>";
+    echo "Hello World!<br>";
+    $x = 5;
+    function myTest($x){
+
+        echo "<p>Variable x  inside function is: $x</p>";
+    }
+
+    echo "<p> Variable x outside function is: $x</p>"
+    ?>
+</body>
+</html>
