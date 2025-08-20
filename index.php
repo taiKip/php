fc506f841a5a33aaebfb53b3aced087fceb8876a
tarus <victortarus6@gmail.com>
2025-08-20 12:35:36 +0930

    functions done

--- index.php
+++ index.php
@@ -1,24 +1,50 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <?php
     $color = "red";
     $txt = "cars";
-    echo "I love ". $txt . "<br>";
-        echo "My car is " . $color . "<br>";
+    echo "I love " . $txt . "<br>";
+    echo "My car is " . $color . "<br>";
     echo "Hello World!<br>";
     $x = 5;
-    function myTest($x){
+    $y = 7;
+    function myTest($x)
+    {
 
         echo "<p>Variable x  inside function is: $x</p>";
+    };
+
+    echo "<p> Variable x outside function is: $x</p>";
+
+    function myTest2()
+    {
+        $GLOBALS['y'] = $GLOBALS['x'] + $GLOBALS['y'];
     }
 
-    echo "<p> Variable x outside function is: $x</p>"
+
+    myTest2();
+
+    echo $y;
+
+    function myTest3(): void
+    {
+        static $x = 0;
+        echo "" . $x . "<br>";
+        $x++;
+    }
+    myTest3();
+    myTest3();
+    myTest3();
+    
     ?>
 </body>
+
 </html>
